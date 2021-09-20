2ad849256b6ed99b387caf64cbef757c6f02a910
Sangeetha Kannan <sangeethakannan96@gmail.com>
2021-09-20 06:00:15 -0700

    First commit

---
+++ JqueryAjaxInMVC/Controllers/EmployeeController.cs
@@ -0,0 +1,108 @@
+﻿using JqueryAjaxInMVC.Models;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.IO;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+
+namespace JqueryAjaxInMVC.Controllers
+{
+    public class EmployeeController : Controller
+    {
+        // GET: Employee
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult ViewAll()
+        {
+         
+            return View(GetAllEmployees());
+        }
+
+        IEnumerable<Employee> GetAllEmployees()
+        {
+            using (JQueryAjaxDBEntities db = new JQueryAjaxDBEntities())
+            {
+                IEnumerable<Employee> emp = db.Employees.ToList();
+                return emp;
+            }
+           
+
+        }
+
+        public ActionResult AddOrEdit(int id=0)
+        {
+            if (id == 0)
+            {
+                Employee emp = new Employee();
+                return View(emp);
+            }
+            else
+            {
+                using (JQueryAjaxDBEntities db = new JQueryAjaxDBEntities())
+                {
+                   Employee emp= db.Employees.Where(x => x.EmployeeID == id).SingleOrDefault();
+                    return View(emp);
+
+                }
+            }
+        }
+
+        [HttpPost]
+        public ActionResult AddOrEdit(Employee emp)
+        {
+            try
+            {
+                if (emp.ImageUpload != null)
+                {
+                    string fileName = Path.GetFileNameWithoutExtension(emp.ImageUpload.FileName);
+                    string extension = Path.GetExtension(emp.ImageUpload.FileName);
+                    fileName = fileName + DateTime.Now.ToString("yymmssfff") + extension;
+                    emp.ImagePath = "~/App_Files/Images/" + fileName;
+                    emp.ImageUpload.SaveAs(Path.Combine(Server.MapPath("~/App_Files/Images/"), fileName));
+                }
+                using (JQueryAjaxDBEntities db = new JQueryAjaxDBEntities())
+                {
+                    if (emp.EmployeeID == 0)
+                    {
+                        db.Employees.Add(emp);
+                        db.SaveChanges();
+                    }
+                    else
+                    {
+                        db.Entry(emp).State = EntityState.Modified;
+                        db.SaveChanges();
+
+                    }
+                    
+                }
+                return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()), message = "Submitted successfully", JsonRequestBehavior.AllowGet });
+
+            }
+            catch(Exception ex)
+            {
+                return Json(new { success = false, message = ex.Message, JsonRequestBehavior.AllowGet });
+
+            }
+        }   
+
+        public ActionResult Delete(int id)
+        {
+            using (JQueryAjaxDBEntities db = new JQueryAjaxDBEntities())
+            {
+                Employee emp = db.Employees.Where(x => x.EmployeeID == id).SingleOrDefault();
+                db.Employees.Remove(emp);
+                db.SaveChanges();
+            }
+            return Json(new { success = true, html = GlobalClass.RenderRazorViewToString(this, "ViewAll", GetAllEmployees()), message = "Deleted successfully", JsonRequestBehavior.AllowGet });
+
+        }
+
+
+    }
+}
---
+++ JqueryAjaxInMVC/Scripts/Scripts.js
@@ -0,0 +1,110 @@
+﻿$(function () {
+    $("#loaderbody").addClass('hide');
+
+
+    $(document).bind('ajaxStart', function () {
+        $("#loaderbody").removeClass('hide');
+    }).bind('ajaxStop', function () {
+        $("#loaderbody").addClass('hide');
+    });
+});
+
+function ShowImagePreview(imageUploader, previewImage) {
+    if (imageUploader.files && imageUploader.files[0]) {
+        var reader = new FileReader();
+        reader.onload = function (e) {
+            $(previewImage).attr('src', e.target.result);
+        }
+        reader.readAsDataURL(imageUploader.files[0]);
+    }
+}
+
+
+function jQueryAjaxPost(form) {
+    $.validator.unobtrusive.parse(form);
+    if ($(form).valid()) {
+
+        var ajaxconfig = {
+
+            type: "POST",
+            url: form.action,
+            data: new FormData(form),
+            success: function (response) {
+
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    refreshAddNewTab($(form).attr('data-restUrl'), true);
+                    $.notify(response.message, "success");
+                    if (typeof activateJqueryDataTable !== 'undefined' && $.isFunction(activateJqueryDataTable))
+                        activateJqueryDataTable();
+                }
+                else {
+                    $.notify(response.message, "error");
+                }
+              
+              
+            }
+        }
+
+        if ($(form).attr("enctype") == "multipart/form-data") {
+            ajaxconfig["contentType"] = false;
+            ajaxconfig["processData"] = false;
+
+        }
+
+        $.ajax(ajaxconfig);
+
+    }
+    return false;
+}
+
+function refreshAddNewTab(resetUrl, showViewTab) {
+  
+    $.ajax({
+        type: "GET",
+        url: resetUrl,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("Add New");
+            if (showViewTab) {
+                $("ul.nav.nav-tabs a:eq(0)").tab("show");
+            }
+        }
+    });
+}
+function Edit(url) {
+    $.ajax({
+
+        type: "GET",
+        url: url,
+        success: function (response) {
+            $("#secondTab").html(response);
+            $("ul.nav.nav-tabs a:eq(1)").html("Edit");
+            $("ul.nav.nav-tabs a:eq(1)").tab("show");
+
+        }
+
+    })
+}
+
+function Delete(url) {
+    if (confirm("Are you sure you want to delete the record?") == true) {
+        $.ajax({
+            type: "POST",
+            url: url,
+            success: function (response) {
+                if (response.success) {
+                    $("#firstTab").html(response.html);
+                    $.notify(response.message, "warn");
+                    if (typeof activateJqueryDataTable !== 'undefined' && $.isFunction(activateJqueryDataTable))
+                        activateJqueryDataTable();
+                }
+
+                else {
+                    $.notify(response.message, "warn");
+                }
+            }
+        })
+    }
+    
+}
